4f91021655bf238323599368fc3e86bb363fac9e
Arthur Lu <root@tronnet.net>
2024-12-01 21:33:02 +0000

    updated hyperbolic perf results 1k docs 768 embed_size over 100 samples

--- search/protocol/get_results.py
+++ search/protocol/get_results.py
@@ -2,11 +2,14 @@ import re, sys
 
 f = open(sys.argv[1]).read()
 
-preprocess = re.findall(r"Preprocessed query to 100-document corpus in: ([0-9].+)s", f)
+preprocess = re.findall(r"Preprocessed query to 1000-document corpus in: ([0-9].+)s", f)
 preprocess = [float(preprocess[i]) for i in range(len(preprocess))]
 
-answer = re.findall(r"Answered query to 100-document corpus in: ([0-9].+)µs", f)
-answer = [float(answer[i]) for i in range(len(answer))]
+a1 = re.findall(r"Answered query to 1000-document corpus in: ([0-9].+)µs", f)
+a2 = re.findall(r"Answered query to 1000-document corpus in: ([0-9].+)ms", f)
+a1f = [float(a1[i]) / 1000 for i in range(len(a1))]
+a2f = [float(a2[i]) for i in range(len(a2))]
+answer = a1f + a2f
 
 upload = re.findall(r"Upload: ([0-9].+) MB", f)
 upload = [float(upload[i]) for i in range(len(upload))]
